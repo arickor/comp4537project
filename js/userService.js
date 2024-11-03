9cc06afc6276a5caa62d8ae7757123b6c927ec66
Diane Choi <ekska21@gmail.com>
2024-11-03 14:34:37 -0800

    Add password reset related functions to UserService

---
+++ js/userService.js
@@ -0,0 +1,115 @@
+const Utils = require('./utils');
+
+// User service for managing user-related database operations
+class UserService {
+  constructor(database) {
+    this.database = database;
+  }
+
+  registerUserWithSecurityQuestion(
+    email,
+    password,
+    securityQuestion,
+    answer,
+    callback
+  ) {
+    const hashedPassword = Utils.hashPassword(password);
+
+    const insertUserQuery = 'INSERT INTO Users (email, password) VALUES (?, ?)';
+    this.database.executeQuery(
+      insertUserQuery,
+      [email, hashedPassword],
+      (err, results) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+
+        const userId = results.insertId;
+        const insertSecurityQuestionQuery =
+          'INSERT INTO SecurityQuestions (user_id, question, answer) VALUES (?, ?, ?)';
+        this.database.executeQuery(
+          insertSecurityQuestionQuery,
+          [userId, securityQuestion, answer],
+          callback
+        );
+      }
+    );
+  }
+
+  getUserByEmail(email, callback) {
+    const query = 'SELECT * FROM Users WHERE email = ?';
+    this.database.executeQuery(query, [email], (err, results) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      if (results.length > 0) {
+        callback(null, results[0]);
+      } else {
+        callback(null, null);
+      }
+    });
+  }
+
+  getSecurityQuestionByEmail(email, callback) {
+    const query = `
+      SELECT question FROM SecurityQuestions
+      JOIN Users ON Users.id = SecurityQuestions.user_id
+      WHERE Users.email = ?
+    `;
+    this.database.executeQuery(query, [email], (err, results) => {
+      if (err || results.length === 0) {
+        callback(err, null);
+      } else {
+        callback(null, results[0].question);
+      }
+    });
+  }
+
+  getSecurityAnswerByEmail(email, callback) {
+    const query = `
+      SELECT answer FROM SecurityQuestions
+      JOIN Users ON Users.id = SecurityQuestions.user_id
+      WHERE Users.email = ?
+    `;
+    this.database.executeQuery(query, [email], (err, results) => {
+      if (err || results.length === 0) {
+        callback(err, null);
+      } else {
+        callback(null, results[0].answer);
+      }
+    });
+  }
+
+  resetPassword(email, newPassword, callback) {
+    const hashedPassword = Utils.hashPassword(newPassword);
+
+    const query = `
+    SELECT Users.id FROM Users
+    WHERE Users.email = ?
+  `;
+    this.database.executeQuery(query, [email], (err, results) => {
+      if (err || results.length === 0) {
+        callback(err, false);
+      } else {
+        const userId = results[0].id;
+        const updatePasswordQuery =
+          'UPDATE Users SET password = ? WHERE id = ?';
+        this.database.executeQuery(
+          updatePasswordQuery,
+          [hashedPassword, userId],
+          (err) => {
+            if (err) {
+              callback(err, false);
+            } else {
+              callback(null, true);
+            }
+          }
+        );
+      }
+    });
+  }
+}
+
+module.exports = UserService;
