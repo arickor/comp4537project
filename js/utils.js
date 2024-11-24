12003b9bfc1cd7d0421b464fab4a970e50887a7e
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-11-23 21:21:31 -0800

    Add parseCookie util file

--- js/utils.js
+++ js/utils.js
@@ -4,6 +4,18 @@ class Utils {
   static hashPassword(password) {
     return crypto.createHash('sha256').update(password).digest('hex');
   }
+
+  static parseCookies(req) {
+    const rawCookies = req.headers.cookie; 
+    if (!rawCookies) return {};
+  
+    return rawCookies.split(';').reduce((cookies, cookie) => {
+      const [name, value] = cookie.split('=').map((c) => c.trim());
+      cookies[name] = decodeURIComponent(value);
+      return cookies;
+    }, {});
+  }
+
 }
 
 module.exports = Utils;
